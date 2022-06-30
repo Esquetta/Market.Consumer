9843b8fc19cd2835089ba74ba92a22cde6d2b0cc
Furkan Taşçı <67598507+Esquetta@users.noreply.github.com>
2022-06-30 12:57:03 +0300

    Models and product controller added

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Market.Consumer.Controllers
@@ -9,10 +10,13 @@ namespace Market.Consumer.Controllers
     public class ProductController : Controller
     {
 
-        
+        private string Url = "https://localhost:44374/";
         public async Task<IActionResult> Index()
         {
-            
+            using (HttpClient httpClient=new HttpClient())
+            {
+                httpClient
+            }
         }
     }
 }
---
+++ Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Market.Consumer.Models
+{
+    public class Product 
+    {
+
+        public int ProductID { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+
+        public string QuantityPerUnit { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public short UnitsInStock { get; set; }
+
+    }
+}
