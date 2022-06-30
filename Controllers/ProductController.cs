878f8202a09da41beba6330cb222b0adc5419293
Furkan Taşçı <67598507+Esquetta@users.noreply.github.com>
2022-06-30 14:34:32 +0300

    Delete product actions finished

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -6,7 +6,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Market.Consumer.Controllers
@@ -25,12 +27,37 @@ namespace Market.Consumer.Controllers
             using (HttpClient httpClient = new HttpClient())
             {
                 var reponse = await httpClient.GetStringAsync(Url + "Product/GetAllProducts");
-                var products = JsonSerializer.Deserialize<List<Product>>(reponse,new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+                var products = System.Text.Json.JsonSerializer.Deserialize<List<Product>>(reponse, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
                 var model = mapper.Map<List<ProductListViewDto>>(products);
                 return View(model);
             }
-            return View();
-
+        }
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var reponse = await httpClient.GetStringAsync(Url + "Product/GetProductById?id=" + id);
+                var product = JsonSerializer.Deserialize<Product>(reponse, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+                if (product != null)
+                {
+                    using (HttpClient deleteClient =new HttpClient())
+                    {
+                        var data =JsonSerializer.Serialize(product);
+                        var httpMessage = new HttpRequestMessage(HttpMethod.Delete, Url+"Product/DeleteProduct")
+                        {
+                            Content = new StringContent(data, Encoding.UTF8, "application/json")
+                        };
+                        var result = await deleteClient.SendAsync(httpMessage);
+                        if (result.IsSuccessStatusCode)
+                        {
+                            TempData.Add("ItemSuccessfully Deleted","Item successfully deleted");
+                            return RedirectToAction("index");
+                        }
+                    }
+                    
+                }
+                return RedirectToAction("index");
+            }
         }
     }
 }
