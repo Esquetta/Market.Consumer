c9d90b2d1e086c36bc4ee8d21fcf620e73b744e3
Furkan Taşçı <67598507+Esquetta@users.noreply.github.com>
2022-06-30 13:21:28 +0300

    Mapper configuration

---
+++ Dtos/ProductListViewDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Market.Consumer.Dtos
+{
+    public class ProductListViewDto
+    {
+        public int productID { get; set; }
+
+        public string productName { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string quantityPerUnit { get; set; }
+
+        public decimal unitPrice { get; set; }
+
+        public short unitsInStock { get; set; }
+    }
+}
--- Helpers/AutoMapperProfiles.cs
+++ Helpers/AutoMapperProfiles.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using AutoMapper;
+using Market.Consumer.Dtos;
+using Market.Consumer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Market.Consumer.Helpers
 {
-    public class AutoMapperProfiles
+    public class AutoMapperProfiles : Profile
     {
-        
+        public AutoMapperProfiles()
+        {
+            CreateMap<Product, ProductListViewDto>().ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.CategoryName));
+        }
     }
 }
